5c4fb532f8ed7909451b81744beb9d598e7eb90f
thanhdaonguyen <110709579+thanhdaonguyen@users.noreply.github.com>
2023-05-16 12:40:52 +0700

    fine-tuning

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+//all boolean virables
+const NORMAL_MODE = 'normal';
+const COLOR_MODE = 'color';
+const BLAKER_MODE ='blacker';
+let mode = NORMAL_MODE;
 
-//the clear button
-const button = document.querySelector('.header .button');
+
+//all the buttons
+const button = document.querySelector('.header .inputbutton'); //the clear button
+const NMbut = document.querySelector('#normalButton');
+const CLbut = document.querySelector('#colorButton');
+const BLbut = document.querySelector('#blackerButton');
+
 //the input slider
 let gridInput = document.querySelector('.gridNumber');
 
@@ -8,11 +18,22 @@ let board = document.querySelector('.board');
 
 //assign createGrid function to the input slider
 gridInput.addEventListener('input', function() {
-    createGrid(Number(gridInput.value));
+    createGrid(gridInput.value);
 });
 
+//assign mode when panel's buttons are pressed
+NMbut.addEventListener('click', () => {
+    mode = NORMAL_MODE;
+    createGrid(gridInput.value);
+});
+CLbut.addEventListener('click', () => {
+    mode = COLOR_MODE;
+    createGrid(gridInput.value);
+});
+BLbut.addEventListener('click', () => mode = BLAKER_MODE);
+
 //check if the mouse is pressed
-let mouseDown = false;
+
 document.body.addEventListener('mousedown', () => {
     mouseDown = true;
 })
@@ -20,6 +41,13 @@ document.body.addEventListener('mouseup', () => {
     mouseDown = false;
 })
 
+//function to return random color
+function getRandomRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+  }
 
 //function to create the grid board
 function createGrid(gridNumber) {
@@ -32,21 +60,43 @@ function createGrid(gridNumber) {
     board.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`
 
     board.innerHTML = '';
-    for (let i = 0; i < gridNumber*gridNumber; i++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('boardsquare');
-        gridItem.addEventListener('mousemove', function() {
-            if (mouseDown) {
+    if (mode == NORMAL_MODE) {
+        for (let i = 0; i < gridNumber*gridNumber; i++) {
+            const gridItem = document.createElement('div');
+            gridItem.classList.add('boardsquare');
+            gridItem.addEventListener('mouseover', function() {
+                if (mouseDown) {
+                    gridItem.classList.add('pixelizedsquare');
+                }
+                
+            });
+            gridItem.addEventListener('mousedown', function() {
                 gridItem.classList.add('pixelizedsquare');
-            }
+            });
+            board.appendChild(gridItem);
             
-        });
-        gridItem.addEventListener('mousedown', function() {
-            gridItem.classList.add('pixelizedsquare');
-        });
-        board.appendChild(gridItem);
-        
+        }
     }
+
+    else if (mode == COLOR_MODE) {
+        for (let i = 0; i < gridNumber*gridNumber; i++) {
+            const gridItem = document.createElement('div');
+            gridItem.classList.add('boardsquare');
+            gridItem.addEventListener('mouseover', function() {
+                if (mouseDown) {
+                    gridItem.style.backgroundColor = `${getRandomRGB()}`;
+                }
+                
+            });
+            gridItem.addEventListener('mousedown', function() {
+                gridItem.style.backgroundColor = `${getRandomRGB()}`;
+            });
+            board.appendChild(gridItem);
+            
+        }
+    }
+    else if (mode == BLAKER_MODE) {}
+    
 }
 
 //show the dimension of the gridboard to the screen
@@ -58,9 +108,12 @@ gridInput.addEventListener('input', function() {
 
 //the clear button
 button.addEventListener('click', function() {
-    const gridboard = document.querySelectorAll('.pixelizedsquare');
-    gridboard.forEach((e) => {
-        e.classList.remove('pixelizedsquare');
-    })
+    createGrid(gridInput.value);
 })
 
+
+
+
+//generate the defaults
+createGrid(gridInput.value);
+text.textContent = `${gridInput.value} x ${gridInput.value}`;
